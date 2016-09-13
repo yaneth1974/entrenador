2a6885c887485239c21b78d71bcfb98981e8caf7
yaneth1974 <roncito_465@hotmail.com>
2016-09-13 14:37:52 +0200

    nuevas actualizaciones en la lista

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 require "vendor/autoload.php";
 
 $app = new Slim\App();//se crea el objeto
+$c = $app->getContainer();
 
-$app->get('/', function($request, $response, $args){
-    $response->write("<h1>Programa Entrenador</h1>");
+$c['view'] = new \Slim\Views\PhpRenderer('plantillas/');
+
+$app->get("/", function($request, $response, $args){
+    $response = $this->view->render($response, "plantilla1.php", []);
     return $response;
-
 });
 
 $app->run();
