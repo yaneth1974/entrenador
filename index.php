b98a3370bea4ace20645b9e8b39419f3212b6c8d
yaneth1974 <roncito_465@hotmail.com>
2016-09-13 11:27:58 +0200

    nuevas actualizaciones en la lista

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ require "vendor/autoload.php";
 
 $app = new Slim\App();//se crea el objeto
 
-$app->get('/saludo', function($request, $response, $args){
-    $response->write("<h1>Hola bienveniods a su programa github</h1>");
+$app->get('/', function($request, $response, $args){
+    $response->write("<h1>Programa Entrenador</h1>");
     return $response;
 
 });
