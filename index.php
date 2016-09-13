d2cec6349aac77cc48ba4f387813997b0cd90c6b
yaneth1974 <roncito_465@hotmail.com>
2016-09-13 12:26:33 +0200

    nuevas actualizaciones en la lista

--- index.php
+++ index.php
@@ -8,11 +8,20 @@ $app->get('/', function($request, $response, $args){
     return $response;
 
 });
+/* Crear tabla Temas */
+        $sql = <<<sql
+create table temas(
+    id int primary key auto_increment,
+    titulo varchar(20) not null
+    titulo_url varchar(40) not null
+);
+sql;
+        $res = $conexion->exec($sql);
+        if($res===FALSE){
+            echo "<p>No se ha podido crear la tabla Temas</p>";
+            echo "<p>".$conexion->errorInfo()[2]."</p>";
+        }else{
+            echo "<p>Tabla temas creada!!!</p>";
+        }
 $app->run();
 ?>
-
-|Temas||Preguntas||Respuestas|
---
-|id||titulo||titulo_url|
-|id||pregunta||pregunta_url||tema|
-|id||respuesta||verdadera||pregunta|
