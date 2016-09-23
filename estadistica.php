f2dca187838d9e0c0b19d94df1e7e760d698a00a
A4Alumno <a4alumno@aules.local>
2016-09-23 12:31:35 +0200

    nuevas

--- estadistica.php
+++ estadistica.php
@@ -3,8 +3,10 @@ class Estadistica{
   public function __invoke($request, $response, $next){
 
     /* Establecer la conexión con el SGBD */
-    $conexion = new PDO("mysql:host=localhost", "root");
+    $conexion = new PDO("mysql:host=localhost;dbname=entrenador", "root");
+    $sql = "insert into estadistica(contador) values(10);";
 
+    $conexion->exec($sql);
 
     return $next($request, $response);
   }
